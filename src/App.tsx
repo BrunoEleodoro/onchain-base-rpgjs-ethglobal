1a63b6004b44d05dd794fb8671250c492512055d
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2025-03-23 02:20:52 -0300

    some more auth basic stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,50 @@
-import { RpgGame } from '@rpgjs/client/react'
-import { RpgClientEngine } from '@rpgjs/client'
-import { RpgServerEngine } from '@rpgjs/server'
-import { SignupPage } from './pages/signup.js'
-import { Web3Wrapper } from './web3/index.js'
-import { useStore } from '@nanostores/react' 
-import { isConnected } from './store/auth.js'
+import { RpgGame } from "@rpgjs/client/react";
+import { RpgClientEngine } from "@rpgjs/client";
+import { Components, RpgServerEngine } from "@rpgjs/server";
+import { SignupPage } from "./pages/signup.js";
+import { Web3Wrapper } from "./web3/index.js";
+import { useStore } from "@nanostores/react";
+import { isConnected } from "./store/auth.js";
+import { useAccount } from "wagmi";
 
+function GameComponent() {
+  const { address } = useAccount();
+
+  const onReady = ({
+    server,
+    client,
+  }: {
+    server: RpgServerEngine;
+    client: RpgClientEngine;
+  }) => {
+    console.log(server, client);
+    console.log("ADDRESS", address);
+    
+  };
+
+  return (
+    <>
+      {address && <RpgGame onReady={onReady} />}
+      {!address && <SignupPage />}
+    </>
+  );
+}
 
 function App() {
   const $isConnected = useStore(isConnected);
 
-  const onReady = ({ server, client }: { server: RpgServerEngine, client: RpgClientEngine }) => {}
-
   return (
     <>
       <Web3Wrapper>
-        {
+        <GameComponent />
+        {/* {
           $isConnected ? (
-            <RpgGame
-              onReady={onReady}
-            />
+            <GameComponent />
           ) : <SignupPage />
-        }
+        } */}
       </Web3Wrapper>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
